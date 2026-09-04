bf4773b0b9f745774613dbcc04bbacb235fcef17
Abdessamad-Bannouf <abdessamad.bannouf@laposte.net>
2026-09-05 01:03:39 +0200

    feat: add a category picker for the quizapi.io import
    - src/api/quizapiCategories.js: getQuizapiCategories()
    - importQuestions() now takes an optional category, appended as a
      query param
    - ImportedQuestionsSection: <select> populated from
      getQuizapiCategories() ("Toutes les catégories" by default), passed
      through to the import call
    
    Requires the backend's category-filtered import
    (ynov-coordination-back#26).

--- src/api/importedQuestions.js
+++ src/api/importedQuestions.js
@@ -1,4 +1,9 @@
 import { api } from './client'
 
 export const getImportedQuestions = () => api.get('/api/imported-questions')
-export const importQuestions = () => api.post('/api/imported-questions/import')
+export const importQuestions = (category) =>
+  api.post(
+    category
+      ? `/api/imported-questions/import?category=${encodeURIComponent(category)}`
+      : '/api/imported-questions/import'
+  )
